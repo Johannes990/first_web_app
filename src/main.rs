8dcc67c62b3e162d6abc879ba259a9f122a322c1
Johannes990 <johannes.jyrgenson@gmail.com>
2024-01-20 12:22:54 +0200

    3rd page

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,18 @@ async fn page_two() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().content_type("text/html").body(fs::read_to_string(path)?))
 }
 
+async fn page_three() -> Result<HttpResponse> {
+    let path = concat!(env!("CARGO_MANIFEST_DIR"), "src/pages/page_three.html");
+    Ok(HttpResponse::Ok().content_type("text/html").body(fs::read_to_string(path)?))
+}
+
 #[actix_web::main]
 async fn main() -> Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
             .route("/page_two", web::get().to(page_two))
+            .route("/page_three", web::get().to(page_three))
     })
         .bind("127.0.0.1:8080")?
         .run()
