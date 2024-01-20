4702aab6e14808c5c94ca4fc564e25e0e2907db0
Johannes990 <johannes.jyrgenson@gmail.com>
2024-01-20 10:46:53 +0200

    page 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, HttpResponse};
+use std::io::{Result};
+
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello, rust web services!!!")
 }
+
+async fn page_two() -> HttpResponse {
+    HttpResponse::Ok().body("site numero dos")
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new().route("/two", web::get().to(page_two))
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
+
+fn main() {}
